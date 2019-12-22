2dff03eac3c20d1a8258f80a91decd714d737a84
Bernd Wichura <wichura@gmx.de>
2019-12-22 20:50:46 +0100

    prepare to get real data, CORS problem not solved

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -2,17 +2,19 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Article } from './article';
 import { ARTICLES } from './mock-articles';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticleService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getArticles(): Observable<Article[]> {
     const articles: Article[] = ARTICLES;
     return of(articles);
+    //return this.http.get<Article[]>("http://52.29.200.187/api/V3/articles?lat=54.35&lng=12&distance=10000000&page=0&size=10");
   }
 
   getArticle(key: string): Observable<Article> {
