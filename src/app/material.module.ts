d274295b17ce289b54ff946fe8a0df37ffc02565
Philipp <philipp.matzel@gmail.com>
2020-09-20 10:49:31 +0200

    Update to Angular 10

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,22 +1,20 @@
 import { NgModule } from '@angular/core';
-import {
-    MatButtonModule,
-    MatMenuModule,
-    MatToolbarModule,
-    MatTooltipModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatCardModule,
-    MatExpansionModule,
-    MatAutocompleteModule,
-    MatDialogModule,
-    MatProgressSpinnerModule,
-    MatCheckboxModule,
-    MatDatepickerModule
-} from '@angular/material';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
     imports: [
